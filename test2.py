3caae7a1542264053bbd1a03c5ab5061b8c12a92
lsy56372 <lsy56372@gmail.com>
2024-01-17 12:07:14 +0000

    1-3

--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
-#123
+from math import sqrt
 #print from 1 to 10
 for i in range(1,11):
     print(i)
@@ -7,3 +7,21 @@ for i in range(1,11):
 for i in range(1,9):
     for j in range(1,9):
         print(f"({i},{j})")    
+
+# distance
+for x0 in range(1,9):
+    for y0 in range(1,9):
+      for x1 in range(1,9):
+          for y1 in range (1,9):
+              if(x0==x1)&(y0==y1):
+                  continue
+              distance=sqrt((x1-x0)**2+(y1-y0)**2)
+      print(f"({x0},{y0})",'to',f"({x1},{y1})",distance)
+
+#4
+
+
+
+
+
+       
