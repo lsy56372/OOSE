9eb9191e90ea63b8735555470a61f934b90e7e96
lsy56372 <lsy56372@gmail.com>
2024-01-17 11:20:26 +0000

    3

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
-
+#123
 #print from 1 to 10
 for i in range(1,11):
     print(i)
 
-#  print chessboard 
+#  print chessboard from(1,1)to(8,8)
 for i in range(1,9):
     for j in range(1,9):
         print(f"({i},{j})")    
