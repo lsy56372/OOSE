e38a34224f745260c736cb9fb0049d4c66c2ec6d
lsy56372 <lsy56372@gmail.com>
2024-01-17 11:19:29 +0000

    2

--- test2.py
+++ test2.py
@@ -1,2 +1,9 @@
-import this
-import this
+
+#print from 1 to 10
+for i in range(1,11):
+    print(i)
+
+#  print chessboard 
+for i in range(1,9):
+    for j in range(1,9):
+        print(f"({i},{j})")    
